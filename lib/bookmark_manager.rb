f238411dcd184749485eb3aa338aa3f49db1c9f0
Dan Bolger <bowedcymbal@gmail.com>
2015-05-18 16:56:09 +0100

    altered file structure to get things working, adds test

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -1,4 +1,20 @@
+require 'data_mapper'
 require 'sinatra/base'
+
+
+#  checking what environment we're in, and defaulting to development:
+env = ENV['RACK_ENV'] || 'development'
+
+# we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './lib/link' # this needs to be done after datamapper is initialised
+
+DataMapper.finalize # After declaring your models, you should finalise them
+
+# However, the database tables don't exist yet. Let's tell datamapper to create them
+DataMapper.auto_upgrade!
+
 
 class BookmarkManager < Sinatra::Base
   get '/' do
@@ -8,3 +24,5 @@ class BookmarkManager < Sinatra::Base
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
+
+
