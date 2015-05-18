67c2ca057642417485558bee3b408aa866849b72
Tim <timrobertson0122@gmail.com>
2015-05-18 18:48:14 +0100

    tag tests passing

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -6,16 +6,30 @@ env = ENV['RACK_ENV'] || 'development'
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 require './lib/models/link' # this needs to be done after datamapper is initialised
+require './lib/models/tag'
+
 DataMapper.finalize # After declaring your models, you should finalise them
 # However, the database tables don't exist yet. Let's tell datamapper to create them
 DataMapper.auto_upgrade!
 
 
 class BookmarkManager < Sinatra::Base
+
   get '/' do
     @links = Link.all
     erb :index
   end
+
+  post '/links' do
+    url = params['url']
+    title = params['title']
+    tags = params['tags'].split(' ').map do |tag| Tag.first_or_create(text: tag) end
+    Link.create(url: url, title: title, tags: tags)
+    redirect to('/')
+  end
+
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
