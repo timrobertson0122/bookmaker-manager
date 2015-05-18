483000f834a68a2230460a377d4bd996109c32aa
Dan Bolger <bowedcymbal@gmail.com>
2015-05-18 13:02:37 +0100

    adds database cleaner

---
+++ spec/link_spec.rb
@@ -0,0 +1,16 @@
+describe Link do
+
+  context 'Demon of how a datamapper works' do
+    it 'should be created and then retrieved from the DB' do
+      expect(Link.count).to eq(0)
+      Link.create(  title: 'Makers Academy',
+                    url: 'http://www.makersacademy.com/')
+      expect(Link.count).to eq(1)
+      link = Link.first
+      expect(link.url).to eq('http://www.makersacademy.com/')
+      expect(link.title).to eq('Makers Academy')
+      link.destroy
+      expect(Link.count).to eq (0)
+    end
+  end
+end
