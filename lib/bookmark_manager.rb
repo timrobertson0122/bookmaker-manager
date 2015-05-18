b41b73382db4ff75eb676806d9dc95affc5e1dcf
Dan Bolger <bowedcymbal@gmail.com>
2015-05-18 17:34:28 +0100

    adds passing first test

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -1,24 +1,20 @@
 require 'data_mapper'
 require 'sinatra/base'
 
-
 #  checking what environment we're in, and defaulting to development:
 env = ENV['RACK_ENV'] || 'development'
-
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-
-require './lib/link' # this needs to be done after datamapper is initialised
-
+require './lib/models/link' # this needs to be done after datamapper is initialised
 DataMapper.finalize # After declaring your models, you should finalise them
-
 # However, the database tables don't exist yet. Let's tell datamapper to create them
 DataMapper.auto_upgrade!
 
 
 class BookmarkManager < Sinatra::Base
   get '/' do
-    'Hello BookmarkManager!'
+    @links = Link.all
+    erb :index
   end
 
   # start the server if ruby file executed directly
