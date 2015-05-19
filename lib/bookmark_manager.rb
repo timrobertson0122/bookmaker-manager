183ede5b53258e375f5072b0758811c2ef5e7768
Tim <timrobertson0122@gmail.com>
2015-05-19 10:48:04 +0100

    passing password_confirmation tests - end of stage 3

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -44,7 +44,8 @@ set :session_secret, 'super secret'
 
   post '/users' do
     user = User.create(email: params[:email],
-                password: params[:password])
+                password: params[:password],
+                password_confirmation: params[:password_confirmation])
     session[:user_id] = user.id
     redirect to ('/')
   end
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -18,7 +18,18 @@ class User
   # has both the password hash and the salt. We save it to the
   # database instead of the plain password for security reasons.
 
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  # this is datamapper's method of validating the model.
+  # The model will not be saved unless both password
+  # and password_confirmation are the same
+  # read more about it in the documentation
+  # http://datamapper.org/docs/validations.html
+  validates_confirmation_of :password
+
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
