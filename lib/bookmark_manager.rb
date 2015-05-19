14e03589b3ff99492b588a52f4301146b236d71b
Tim <timrobertson0122@gmail.com>
2015-05-19 08:47:31 +0100

    stage 2 completed

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -52,8 +52,9 @@ set :session_secret, 'super secret'
   helpers do
 
     def current_user
-      @
-
+      @current_user ||= User.get(session[:user_id]) if session[:user_id]
+    end
+end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
