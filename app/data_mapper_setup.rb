9e437ad67c9766a0b7bee5e50c37376fd8acd48a
Tim <timrobertson0122@gmail.com>
2015-05-20 17:24:16 +0100

    heroku testing

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,7 +2,7 @@
 #  checking what environment we're in, and defaulting to development:
 env = ENV['RACK_ENV'] || 'development'
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 
 require_relative 'models/link.rb'
 require_relative 'models/tag.rb'
